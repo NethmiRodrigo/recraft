a7d107e4a3bef0be30d0c688e67ec0dd9f713e16
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2020-10-25 13:56:02 +0530

    Removed database url

--- backend/util/admin.js
+++ backend/util/admin.js
@@ -1,15 +1,11 @@
-var admin  = require('firebase-admin')
+var admin = require("firebase-admin");
 
 const serviceAccount = require("./serviceAccount.json");
 
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://thumbsup-dd6b9.firebaseio.com",
+	credential: admin.credential.cert(serviceAccount),
 });
-
 
 const db = admin.firestore();
 
-module.exports = {admin, db};
-
+module.exports = { admin, db };
